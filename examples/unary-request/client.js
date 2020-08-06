9cfd3024109c80644be782886891aaa56c05bd30
Elon Salfati <elon@salfati.io>
2020-08-06 15:44:38 +0300

    fixed example

--- examples/unary-request/client.js
+++ examples/unary-request/client.js
@@ -3,17 +3,7 @@ const { client } = require("../../")
 
 // send dynamic data
 client()
-  .send(
-    "localhost:3000/Greeter/SayHello",
-    {
-      name: "pelerin"
-    },
-    {
-      headers: {
-        Authorization: `Basic 123456`
-      }
-    }
-  )
+  .send("localhost:3000/Greeter/SayHello", { name: "pelerin" })
   .then((response) => console.log(response))
   .catch((err) => console.error(err))
 
